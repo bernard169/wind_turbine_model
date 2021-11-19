d013c4cc37eeff51a6be3128c062f7534b1f3941
Bernard Tourneur <36165894+bernard169@users.noreply.github.com>
2021-11-19 13:05:33 +0100

    first commit

---
+++ model.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+
+Uinf = # Mean Wind Speed
+a = # Axial Induction Factor
+omega = # Angular velocity
+r = 126 # Radius
+aprime = # rotational induction factor
+phi = # Local flow angle
+alpha = # Local angle of attack
+beta = # Local element pitch angle
+c = # Chord length
+B = 3 # Number of blades
+rho = # Air density
+Vres = # Velocity seen by the blades
+Cl = # Lift coefficient
+Cd = # Drag coefficient
+Q = # Torque
+T = # Thrust
